9d2fdf8dd022acf6072534c975672fb1051ce907
Jacob Parker <jacobparker1992@gmail.com>
2016-09-28 09:49:12 +0100

    Initial Commit

---
+++ ScrabbleTests/ScrabbleTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScrabbleTests.swift
+//  ScrabbleTests
+//
+//  Created by Jacob Parker on 28/09/2016.
+//  Copyright © 2016 Jacob Parker. All rights reserved.
+//
+
+import XCTest
+@testable import Scrabble
+
+class ScrabbleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
